2ec4a11a20eefff30cb0651aa6c4fdb9feb75b96
sumaiya <sumaiyarimu22@gmail.com>
2022-12-28 08:23:13 +0600

    navber added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
+import Footer from "./components/Footer";
 const App = () => {
-  return <div>app</div>;
+  return (
+    <>
+      <div className="app min-h-screen bg-gray-50 text-gray-700">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import { BsCart3 } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-bg bg-cyan-700  text-violet-50 h-20 flex justify-center items-center">
+      <div className="navbar container mx-auto flex items-center justify-between">
+        <div className="left">
+          <span className="text-xl font-semibold">
+            easy<span className="text-orange-500">Tech</span>
+          </span>
+        </div>
+        <div className="right flex items-center gap-5">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/products" className="nav-link">
+            Products
+          </Link>
+          <Link to="/cart">
+            <span className="cart-icon relative">
+              <BsCart3 />
+              <span className="cart-counter absolute -top-3 -right-3 text-xs bg-orange-600 h-5 w-5 rounded-full flex items-center justify-center font-medium">
+                10
+              </span>
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
